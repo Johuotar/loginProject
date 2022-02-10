96e8491315102b72ae86a771930359fe55afddb3
Johuotar <jmh@dnainternet.net>
2022-02-10 13:32:08 +0200

    fix nested repo

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+//React Router v6 introduces a Routes component which replaces Switch
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Register from "./Register";
+
+function App() {
+  return (
+      <BrowserRouter>
+
+      <div>
+        <Link to={'/home'}>Home</Link> |
+        <Link to={'/login'}>Login</Link> |
+        <Link to={'/register'}>Register</Link>
+      </div>
+      <Routes>
+        <Route exact path={'/register'} element={<Register />}>
+        </Route>
+      </Routes>
+      <hr />
+      
+      </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/Register.js
@@ -0,0 +1,12 @@
+function Register() {
+    return (
+        <form action="">
+            <h2>Register</h2>
+            <input type="email" placeholder=""/><br />
+            <input type="password" placeholder="password"/><br />
+            <button type="submit">register</button>
+        </form>
+    )
+}
+
+export default Register;
